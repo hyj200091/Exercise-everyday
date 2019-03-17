548e18ce39344c2e2a42332c117b24d779750bdf
侯粤嘉 <2298090131@qq.com>
2019-03-18 06:49:23 +0800

    date

--- src/Exercise6/StringTest.java
+++ src/Exercise6/StringTest.java
@@ -1,9 +1,7 @@
 package Exercise6;
 
-
-
 /**
- * Stringn StringBuffet ,StringBuildD的比较
+ * Stringn ，StringBuffet ,StringBuildD的比较
  * author 侯粤嘉
  * 2019.3.16
  */
@@ -20,7 +18,7 @@ public class StringTest {
       //获取一毫秒为单位的当前开始时间
         long begin = System.currentTimeMillis();
         //调用50000次Stringde的拼接操作，每次都会生成新的String对象；
-        for (int i = 5;i<time ;i++){
+        for (int i = 0;i<time ;i++){
             s +="java";
         }
         //获取以毫秒为单位的单前结束时间
@@ -31,7 +29,7 @@ public class StringTest {
     public static void testStringBuffer(){
         StringBuffer stringBuffer = new StringBuffer();
         long begin =System.currentTimeMillis();
-        for (int i =5;i<time;i++){
+        for (int i =0;i<time;i++){
             stringBuffer.append("java");
         }
         long over = System.currentTimeMillis();
@@ -40,7 +38,7 @@ public class StringTest {
     public static void testStringBuilder(){
         StringBuilder stringBuilder = new StringBuilder();
         long begin = System.currentTimeMillis();
-        for (int i = 5;i<time;i++){
+        for (int i = 0;i<time;i++){
             stringBuilder.append("java");
         }
         long over = System.currentTimeMillis();
